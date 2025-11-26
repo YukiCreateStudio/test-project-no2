90ffd768fd7b2edf3dafb6ae435e098570d50337
YukiCreateStudio <yuki.createstudio@gmail.com>
2025-11-26 12:35:38 +0900

    Ｐ５０まで

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,17 @@
-import type { Metadata } from "next";
 import "./globals.css";
-
-export const metadata: Metadata = {
-  title: "Test Project No.2",
-  description: "Test Project No.2",
-};
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="ja">
-      <body>{children}</body>
+      <body>
+        {/* Header挿入予定 */}
+        <main>{children}</main>
+        {/* Footer挿入予定 */}
+      </body>
     </html>
   );
 }
